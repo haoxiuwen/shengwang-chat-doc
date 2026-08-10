2fb7b6acd540e14cb32d9aeeb57752d9f922afb7
haoxiuwen <amy.hao@easemob.com>
2026-08-10 17:10:34 +0800

    Add unified integration doc for applet platform and remove docs for separate environments

--- docs/.vuepress/sidebar/document.ts
+++ docs/.vuepress/sidebar/document.ts
@@ -198,11 +198,7 @@ const documentV5Sidebar = [
         text: '集成介绍',
         collapsible: true,
         children: [
-          { text: '微信小程序', link: 'wechat.html' },
-          { text: 'QQ 小程序', link: 'qq.html' },
-          { text: '百度小程序', link: 'baidu.html' },
-          { text: '抖音小程序', link: 'bytedance.html' },
-          { text: '支付宝小程序', link: 'alipay.html' },
+          { text: '集成小程序 SDK', link: 'integration.html' },
           { text: 'Uniapp 全平台', link: 'uniapp.html' },
         ],
         only: ['applet']
