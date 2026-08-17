71bf11af29a1b99a6cb32e702da20e29a6d6a102
haoxiuwen <amy.hao@easemob.com>
2026-08-17 13:51:53 +0800

    Remove Push Tag Console Doc and Modify Other Push Docs

--- docs/.vuepress/sidebar/push.ts
+++ docs/.vuepress/sidebar/push.ts
@@ -8,24 +8,13 @@ export const PUSH_SIDEBAR = [
         { text: '海外计费说明', link: 'push_billing_overseas.html' },
       ]
     },
-    {
-      text: '快速入门',
-      collapsible: true,
-      children: [
-        { text: '开通服务', link: 'push_activate.html' },
-        { text: '证书配置', link: 'push_certificate_config.html' },
-        { text: '创建推送', link: 'push_task_create.html' },
-        { text: '推送任务', link: 'push_task.html' },
-        { text: '标签管理', link: 'push_tag_mgmt.html' },
-        { text: '数据统计', link: 'push_statistics.html' },
-      ]
-    },
+    { text: '开通服务', link: 'push_activate.html' },
+    { text: '证书配置', link: 'push_certificate_config.html' },
     {
       text: 'Android 接入',
       collapsible: true,
       children: [
         { text: '推送集成说明', link: 'push_integration_note_android' },
-        { text: '推送集成过程', link: 'push_integration_process_android' },
         { text: '厂商统计', link: 'push_androidvendor_statistics' },
         // { text: '推送厂商消息分类', link: 'push_androidvendor_msgclassification' },
         { text: '厂商通道限制及解决方案', link: 'push_androidchannel_restriction.html' },
@@ -36,7 +25,6 @@ export const PUSH_SIDEBAR = [
       collapsible: true,
       children: [
         { text: '推送集成说明', link: 'push_integration_note_ios' },
-        { text: '推送集成过程', link: 'push_integration_process_ios' },
         { text: 'APNs 送达统计', link: 'push_apns_deliver_statistics.html' },
       ]
     },
